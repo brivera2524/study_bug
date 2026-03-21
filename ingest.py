d1d0f2b16425caa31d6cd72b38b26ec547a4b8fd
Brian Rivera <brivera2524@gmail.com>
2026-03-21 00:54:59 -0700

    refactor: wrap ingestion pipeline in Embedder class

--- ingest.py
+++ ingest.py
@@ -7,102 +7,113 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 
 
+class Embedder():
+    
 
-# create the embedding function with GPU for ingest
-ef = SentenceTransformerEmbeddingFunction(
-    model_name="all-MiniLM-L6-v2",
-    device="cuda"
-)
-
-# pass it to the collection
-client = chromadb.PersistentClient(path="./chroma_data")
-collection = client.get_or_create_collection(
-    name="textbook",
-    embedding_function=ef
-)
-
-
-textbook_names = ["textbook_0.pdf"]
-pdf_dir = Path("textbooks/pdf")
-cache_dir = Path("textbooks/cache")
-
-cache_dir.mkdir(parents=True, exist_ok=True)
-
-
-# Check first for cached version, load and cache if doesn't exist
-for textbook_name in textbook_names:
-    pdf_file = pdf_dir / textbook_name
-    cache_file = cache_dir / (Path(textbook_name).stem + ".json")
-
-    if not cache_file.exists():
-        chunks = pymupdf4llm.to_markdown(str(pdf_file), page_chunks=True)
-        cache_file.write_text(
-            json.dumps(chunks, ensure_ascii=False, indent=2),
-            encoding="utf8"
+    def __init__(self, db_path: Path, collection_name: str, data_dir: Path):
+        self.client = chromadb.PersistentClient(path=db_path)
+        ef = SentenceTransformerEmbeddingFunction(
+            model_name="all-MiniLM-L6-v2",
+            device="cuda"
+            )
+        self.collection = self.client.get_or_create_collection(
+            name=collection_name,
+            embedding_function=ef
+            )
+        self.splitter = RecursiveCharacterTextSplitter(
+            chunk_size=512,
+            chunk_overlap=64
         )
+        self.pdf_dir = data_dir / "pdf"
+        self.cache_dir = data_dir / "cache"
+        self.cache_dir.mkdir(parents=True, exist_ok=True)
+        self.BATCH_SIZE = 500
 
-    chunks = json.loads(cache_file.read_text(encoding="utf8"))
-
-
-# Currently one chunk per page, reduce further for more specific retrieval
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=512,
-    chunk_overlap=64
-)
-
-final_chunks = []
-for chunk in chunks:
-    page_text = chunk["text"]
-    page_number = chunk["metadata"]["page_number"]
-    source = chunk["metadata"]["file_path"]
     
-    sub_chunks = splitter.split_text(page_text)
-    
-    for i, sub_chunk in enumerate(sub_chunks):
-        final_chunks.append({
-            "text": sub_chunk,
-            "page": page_number,
-            "source": source,
-            "chunk_index": i
-        })
+    def process_docs_to_chunks(self): 
+        cached_stems = {f.stem for f in self.cache_dir.glob("*.json")}
+
+        all_chunks = []
+
+        for pdf_file in self.pdf_dir.glob("*.pdf"):
+            cache_file = self.cache_dir / (pdf_file.stem + ".json")
+
+            if pdf_file.stem not in cached_stems:
+                chunks = pymupdf4llm.to_markdown(str(pdf_file), page_chunks=True)
+                cache_file.write_text(
+                    json.dumps(chunks, ensure_ascii=False, indent=2),
+                    encoding="utf8"
+                )
+                print(f"Cached {pdf_file.name}")
+            else:
+                print(f"{pdf_file.name} Already cached.")
+
+            chunks = json.loads(cache_file.read_text(encoding="utf8"))
+            all_chunks.append(chunks)
+
+        return all_chunks
 
 
-# add in batches to respect ChromaDB's limit
-BATCH_SIZE = 500
 
-ids = [str(uuid.uuid4()) for _ in final_chunks]
-documents = [chunk["text"] for chunk in final_chunks]
-metadatas = [{
-    "chunk_index": chunk["chunk_index"],
-    "page": chunk["page"],
-    "source": chunk["source"]
-} for chunk in final_chunks]
+    def embed_all_chunks(self, all_chunks: list[dict]):
+        final_chunks = []
 
-for i in range(0, len(final_chunks), BATCH_SIZE):
-    collection.add(
-        ids=ids[i:i + BATCH_SIZE],
-        documents=documents[i:i + BATCH_SIZE],
-        metadatas=metadatas[i:i + BATCH_SIZE]
+        for chunk_dict in all_chunks:
+            for chunk in chunk_dict:
+                page_text = chunk["text"]
+                page_number = chunk["metadata"]["page_number"]
+                source = chunk["metadata"]["file_path"]
+                
+                sub_chunks = self.splitter.split_text(page_text)
+                
+                for i, sub_chunk in enumerate(sub_chunks):
+                    final_chunks.append({
+                        "text": sub_chunk,
+                        "page": page_number,
+                        "source": source,
+                        "chunk_index": i
+                    })
+
+        ids = [str(uuid.uuid4()) for _ in final_chunks]
+        documents = [chunk["text"] for chunk in final_chunks]
+        metadatas = [{
+            "chunk_index": chunk["chunk_index"],
+            "page": chunk["page"],
+            "source": chunk["source"]
+        } for chunk in final_chunks]
+
+        for i in range(0, len(final_chunks), self.BATCH_SIZE):
+            self.collection.add(
+                ids=ids[i:i + self.BATCH_SIZE],
+                documents=documents[i:i + self.BATCH_SIZE],
+                metadatas=metadatas[i:i + self.BATCH_SIZE]
+            )
+            print(f"Added chunks {i} to {min(i + self.BATCH_SIZE, len(final_chunks))}")
+
+        print(f"Total chunks stored: {self.collection.count()}")
+
+
+if __name__ == "__main__":
+
+    embedder = Embedder(db_path=Path("chroma_data/"), collection_name="textbooks", data_dir=Path("textbooks/"))
+    all_chunks = embedder.process_docs_to_chunks()
+    embedder.embed_all_chunks(all_chunks)
+
+    query = ["What is the reversal agent for opiods?"]
+
+    results = embedder.collection.query(
+        query_texts=query,
+        n_results=10
     )
-    print(f"Added chunks {i} to {min(i + BATCH_SIZE, len(final_chunks))}")
 
-print(f"Total chunks stored: {collection.count()}")
+    # access the results for
+    docs = results["documents"][0]
+    metas = results["metadatas"][0]
+    distances = results["distances"][0]
 
-query = "What is the reversal agent for opiods?"
-
-results = collection.query(
-    query_texts=query,
-    n_results=10
-)
-
-# access the results for
-docs = results["documents"][0]
-metas = results["metadatas"][0]
-distances = results["distances"][0]
-
-# print them out together
-for doc, meta, distance in zip(docs, metas, distances):
-    print(f"Distance: {distance:.4f}")
-    print(f"Page: {meta['page']} | Source: {meta['source']}")
-    print(f"Text: {doc[:300]}")
-    print("---")
+    # print them out together
+    for doc, meta, distance in zip(docs, metas, distances):
+        print(f"Distance: {distance:.4f}")
+        print(f"Page: {meta['page']} | Source: {meta['source']}")
+        print(f"Text: {doc[:300]}")
+        print("---")
